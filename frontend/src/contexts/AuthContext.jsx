6104374e7b7b3e994cb1668e31aa8c59abffeac9
Abhijeet.M <abhijeetiyer07@gmail.com>
2026-03-01 16:15:27 +0530

    Update axios baseURL for production

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -2,16 +2,14 @@ import React, { useState, createContext, useContext, useEffect } from "react";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
 const client = axios.create({
-  baseURL: "http://localhost:8000/api/v1/users",
-  withCredentials: true, 
+  baseURL: `${import.meta.env.VITE_API_URL}/api/v1/users`,
+  withCredentials: true,
 });
-
 export const AuthContext = createContext(null);
 
 export const AuthProvider = ({ children }) => {
-
   const [userData, setUserData] = useState(null);
-  const [loading, setLoading] = useState(true); 
+  const [loading, setLoading] = useState(true);
   const navigate = useNavigate();
 
   const handleRegister = async (name, userName, password) => {
@@ -47,17 +45,16 @@ export const AuthProvider = ({ children }) => {
   const handleLogout = async () => {
     try {
       await client.post("/logout");
-      setUserData(null); 
+      setUserData(null);
       navigate("/auth");
     } catch (error) {
       console.error("Logout error:", error.response?.data || error.message);
     }
   };
-  
+
   useEffect(() => {
     const checkUserStatus = async () => {
       try {
-        
         const response = await client.get("/status");
         if (response.data.user) {
           setUserData(response.data.user);
@@ -65,7 +62,6 @@ export const AuthProvider = ({ children }) => {
       } catch (error) {
         console.log("User not authenticated.");
       } finally {
-
         setLoading(false);
       }
     };
@@ -73,13 +69,12 @@ export const AuthProvider = ({ children }) => {
     checkUserStatus();
   }, []);
 
-
   const value = {
     userData,
-    loading, 
+    loading,
     handleRegister,
     handleLogin,
-    handleLogout, 
+    handleLogout,
   };
 
   return (
